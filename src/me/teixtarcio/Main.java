34ddb3e0997da68863f82b1bd4c6800bd32209dd
Tárcio T <108308831+teixtarcio@users.noreply.github.com>
2022-08-04 14:46:55 -0300

    initial commit

---
+++ src/me/teixtarcio/Main.java
@@ -0,0 +1,15 @@
+package me.teixtarcio;
+
+public class Main extends JavaPlugin{
+
+	@Override
+	public void onEnable(){
+		System.out.println("minecraft-spigot119 - Plugin setup enabled!");
+	}
+	
+	@Override
+	public void onDisable(){
+		System.out.println("minecraft-spigot119 - Plugin setup disabled!");
+
+	}
+}
